aad480184291137d345855d5ad953be6ca24ec4f
jaemnkm <jaemnkm@gmail.com>
2019-12-07 20:57:51 -0800

    Updates

---
+++ precache-manifest.1451c5c9b85d8bca959f5032b1715497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00374bb0944615a529ad9663ea38921d",
+    "url": "/airport_kiosk/index.html"
+  },
+  {
+    "revision": "cd2d6d02845ba38115bb",
+    "url": "/airport_kiosk/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e6c770d4a11d0ab9829e",
+    "url": "/airport_kiosk/static/js/2.69b76237.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/airport_kiosk/static/js/2.69b76237.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd2d6d02845ba38115bb",
+    "url": "/airport_kiosk/static/js/main.f59e6090.chunk.js"
+  },
+  {
+    "revision": "30ae5979bc65a614f8b3",
+    "url": "/airport_kiosk/static/js/runtime-main.7b463ca9.js"
+  }
+]);
